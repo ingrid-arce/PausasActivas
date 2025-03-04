5dfaa6513ffff46c929619a2a8b1a76f513dc1ed
ingrid-arce <ajulieth918@gmail.com>
2025-03-04 16:02:04 -0500

    cambios en el color del header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useNavigate} from "react-router-dom";
 import "./HeaderStyle.css"; 
 
 
 const Header1 = () => {
+
+    const navigate = useNavigate();
     return (
         <header className="Header">
-            <h1>GreenSQA</h1>
+            <h1  className="title2"
+            onClick={()=> navigate("/")}> GreenSQA</h1>
         </header>
     );
 };
